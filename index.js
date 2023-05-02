3e2f6a879af6c15e2b8f15279fc373315a8a726a
Samuel <samuelfras@gmail.com>
2023-05-01 20:27:56 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement("h1")
+newHeader.textContent = 'HornedRat is the champion'
+newHeader.id = 'victory'
+console.log(document.body)
+document.body.append(newHeader)
